8162c400b3262583c444cb56860dac512c4cd4f9
a1dnan <aaliladnane@gmail.com>
2024-02-03 22:36:39 +0100

    add customer-front-angular

---
+++ customer-front-angular-app/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {KeycloakProfile} from "keycloak-js";
+import {KeycloakService} from "keycloak-angular";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'customer-front-angular-app';
+  public profile? : KeycloakProfile;
+  constructor(public keycloakService:KeycloakService) {
+  }
+  async ngOnInit() {
+    if (await this.keycloakService.isLoggedIn()) {
+      this.keycloakService.loadUserProfile().then(profile => {
+        this.profile = profile;
+      });
+    }
+  }
+
+  async login() {
+    await this.keycloakService.login({
+      redirectUri: window.location.origin
+    });
+  }
+
+  logout() {
+    this.keycloakService.logout(window.location.origin)
+  }
+}
---
+++ customer-front-angular-app/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import {APP_INITIALIZER, NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { CustomersComponent } from './customers/customers.component';
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+import {HttpClientModule} from "@angular/common/http";
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'customers-realm',
+        clientId: 'customers-client-angular'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        checkLoginIframe : true,
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent,
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    KeycloakAngularModule
+  ],
+  providers: [
+    {provide : APP_INITIALIZER, deps : [KeycloakService],useFactory : initializeKeycloak, multi : true}
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
